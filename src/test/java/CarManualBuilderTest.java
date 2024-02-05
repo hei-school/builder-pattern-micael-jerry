92d0dc29f4710895b28052cc33957f9101d409c8
Micael Jerry <hei.jerry.2@gmail.com>
2024-02-05 19:12:56 +0300

    test: update test

--- src/test/java/CarManualBuilderTest.java
+++ src/test/java/CarManualBuilderTest.java
@@ -1,6 +1,10 @@
+import org.dp.builder.CarBuilder;
 import org.dp.builder.CarManualBuilder;
 import org.dp.director.Director;
+import org.dp.model.Car;
 import org.dp.model.Manual;
+import org.dp.model.component.Engine;
+import org.dp.model.component.TripComputer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +12,18 @@ import static utils.Util.MANUAL_SPORTIVE_CAR;
 import static utils.Util.MANUAL_SUV;
 
 public class CarManualBuilderTest {
+
+    @Test
+    public void testCarManualBuilder() {
+        CarManualBuilder carManualBuilder = new CarManualBuilder();
+        carManualBuilder.setEngine(new Engine(6));
+        carManualBuilder.setTripComputer(new TripComputer());
+
+        Manual result = carManualBuilder.getResult();
+        Assertions.assertEquals(new Engine(6), result.getEngine());
+        Assertions.assertEquals(result.getTripComputer(), result.getTripComputer());
+    }
+
     @Test
     public void testCarManualBuilderWithDirector_1() {
         Director director = new Director();
