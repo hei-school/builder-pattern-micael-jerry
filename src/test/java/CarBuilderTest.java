a97490d755d700b9a87f3b42f4eaefe4ebd1a8bb
Micael Jerry <hei.jerry.2@gmail.com>
2024-02-05 19:11:24 +0300

    test: update test

--- src/test/java/CarBuilderTest.java
+++ src/test/java/CarBuilderTest.java
@@ -1,6 +1,8 @@
 import org.dp.builder.CarBuilder;
 import org.dp.director.Director;
 import org.dp.model.Car;
+import org.dp.model.component.Engine;
+import org.dp.model.component.TripComputer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +10,17 @@ import static utils.Util.CAR_SPORTIVE_CAR;
 import static utils.Util.CAR_SUV;
 
 public class CarBuilderTest {
+
+    @Test
+    public void testCarBuilder() {
+        CarBuilder carBuilder = new CarBuilder();
+        carBuilder.setEngine(new Engine(6));
+        carBuilder.setTripComputer(new TripComputer());
+
+        Car result = carBuilder.getResult();
+        Assertions.assertEquals(new Engine(6), result.getEngine());
+        Assertions.assertEquals(result.getTripComputer(), result.getTripComputer());
+    }
 
     @Test
     public void testCarBuilderWithDirector_1() {
